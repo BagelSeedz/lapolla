cdea0330567eb2cecd37a2a1eef02f71cd63c797
BagelSeedz <87871784+BagelSeedz@users.noreply.github.com>
2026-04-02 21:38:34 -0400

    added new qualified teams

--- lapolla-app/src/ScoreInputPage.jsx
+++ lapolla-app/src/ScoreInputPage.jsx
@@ -14,7 +14,6 @@ class ScoreImportPage extends React.Component {
         }
     }
 
-
     render() {
         const teams = groups[this.state.group];
         const matchData = matchesJSON[this.state.group];
